ecab8ec4eef8e0459e272bad6843b472a6e68310
WenTing <mybaby0103066@gmail.com>
2020-12-10 16:17:08 +0800

    完成跑馬燈功能

---
+++ front/marquee.php
@@ -0,0 +1,20 @@
+<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+<?php
+$ads=$Ad->all(['sh'=>1]);
+$str="";
+foreach($ads as $ad){
+  $str=$str.$ad['text']."&nbsp;";
+
+}
+echo $str;
+
+
+// 可以寫成
+// foreach($ads as $ad){
+//   echo $ad['text']."&nbsp;&nbsp;&nbsp;";
+
+// }
+
+?>
+
+</marquee>
