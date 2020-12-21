c6c4740401d8b24330237f85495d7268cb0e3a9a
WenTing <mybaby0103066@gmail.com>
2020-12-21 13:40:43 +0800

    完成最新文章顯示及分頁和切換詳細內容功能

--- front/news.php
+++ front/news.php
@@ -7,12 +7,19 @@
       <td width="20%"></td>
     </tr>
     <?php
-      $all=$News->all(['sh'=>1]);
+      $count=$News->count(['sh'=>1]);
+      $div=5;
+      $pages=ceil($count/$div);
+      $now=(isset($_GET['p']))?$_GET['p']:1;
+      $start=($now-1)*$div;
+
+    
+      $all=$News->all(['sh'=>1]," limit $start,$div");
       foreach($all as $news){
 
     ?>
     <tr>
-      <td id="t<?=$news['id'];?>"><?=$news['title'];?></span></td>
+      <td class="header" id="t<?=$news['id'];?>" style="cursor:pointer;color:blue;text-decoration:underline"><?=$news['title'];?></span></td>
       <td>
         <span class="title"><?=mb_substr($news['text'],0,30,'utf8');?>...</span>
         <span class="text" style="display:none"><?=nl2br($news['text']);?></span>
@@ -23,5 +30,27 @@
       }
     ?>
   </table>
+  <div class="ct">
+      <?php
+      if(($now-1)>0){
+        echo "<a href='index.php?do=news&p=".($now-1)."'> &lt; </a>";
+      }
+      for($i=1;$i<=$pages;$i++){
+        $fontsize=($i==$now)?"28px":"18px";
+        echo "<a href='index.php?do=news&p=$i' style='font-size:$fontsize'> $i </a>";
+      }
+      if(($now+1)<=$pages){
+      echo "<a href='index.php?do=news&p=".($now+1)."'> &gt; </a>";
+      }
+      ?>
+  </div>
+  <div class="ct"><input type="submit" value="確定修改"></div>
 
 </fieldset>
+
+<script>
+$(".header").on("click",function(){
+  $(this).next().children('.title').toggle();  //toggle 點擊後讓原本為顯示的區塊變為隱藏，原為隱藏的區塊變為顯示
+  $(this).next().children('.text').toggle();
+})
+</script>
