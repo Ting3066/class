2434b2eefc0544b53ce6cd0d427ea88aa9910196
WenTing <mybaby0103066@gmail.com>
2020-12-30 16:10:12 +0800

    完成預告片後台各項功能

---
+++ api/edit_poster.php
@@ -0,0 +1,18 @@
+<?php
+include_once "../base.php";
+// print_r($_POST['id']);
+foreach($_POST['id'] as $key => $id){
+  if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+    $Poster->del($id);
+  }else{
+    $poster=$Poster->find($id);
+    $poster['name']=$_POST['name'][$key];
+    $poster['ani']=$_POST['ani'][$key];
+    $poster['sh']=in_array($id,$_POST['sh'])?1:0;
+    $Poster->save($poster);
+  }
+}
+
+to("../backend.php?do=poster");
+
+?>
