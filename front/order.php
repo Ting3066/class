c4b2ecf89fe907ec5abf34fb521956525c15f754
WenTing <mybaby0103066@gmail.com>
2021-01-11 11:28:29 +0800

    完成取得可訂票場次功能

--- front/order.php
+++ front/order.php
@@ -7,7 +7,7 @@
     </tr>
     <tr>
       <td>日期:</td>
-      <td><select name="date" id="date" style="width:98%"></select></td>
+      <td><select name="date" id="date" style="width:98%" onchange="getSessions()"></select></td>
     </tr>
     <tr>
       <td>場次:</td>
@@ -45,7 +45,7 @@ function getMovies(id){
   }else{
     movie="all";
   }
-  console.log(movie);
+  // console.log(movie);
   $.get("api/get_movies.php",{movie},function(movies){
     $("#movie").html(movies);
     getDays();
@@ -53,10 +53,19 @@ function getMovies(id){
 }
 
 function getDays(){
-  let movie=$("#movie").val();
+  let movie=$("#movie").val();  //取得目前電影選單選中的電影id
   $.get("api/get_days.php",{movie},function(days){
     $("#date").html(days);
+    getSessions();
   });
+}
+
+function getSessions(){
+  let movie=$("#movie").val();
+  let date=$("#date").val();
+  $.get("api/get_sessions.php",{movie,date},function(sessions){
+    $("#session").html(sessions);
+  })
 }
 
 </script>
