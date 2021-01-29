e2a8f78083f2b667a73f4466c1661f9aef94ac05
WenTing <mybaby0103066@gmail.com>
2021-01-29 13:22:20 +0800

    修正上傳檔案問題

--- api/edit_goods.php
+++ api/edit_goods.php
@@ -4,6 +4,9 @@ include_once "../base.php";
 
 if(!empty($_FILES['img']['tmp_name'])){
   $_POST['img']=$_FILES['img']['name'];
+}else{
+  $goods=$Goods->find($_POST['id']);
+  $_POST['img']=$goods['img'];
 }
 
 
